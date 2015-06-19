01f94931138834afe073a24fa31eb66b8ef05abe
PedroLDN <pedrofrankstein13@hotmail.com>
2015-06-19 20:12:11 -0300

    Função "generateTable" concluída
    A função que cria a tabela e os dados dela, foi concluída.

--- js/main_script.js
+++ js/main_script.js
@@ -1,9 +1,9 @@
-"use strict"; // Usa o modo strict do ECMAScript 5.
+Ôªø"use strict"; // Usa o modo strict do ECMAScript 5.
 
 window.onload = function(){
 
-	// Quando o programa È iniciado, recupera os antigos valores, ou 
-	// ent„o apenas inicializa o array.
+	// Quando o programa √© iniciado, recupera os antigos valores, ou 
+	// ent√£o apenas inicializa o array.
 	var ls = window.localStorage;
 	if(ls.table_columns){
 	    var table_columns = JSON.parse(ls.table_columns);
@@ -19,64 +19,77 @@ window.onload = function(){
 	    var table_products = [];
 	}
 
-	// Essa funÁ„o cria a tabela com os dados que o programa j·
+	// Essa fun√ß√£o cria a tabela com os dados que o programa j√°
 	// possui. Tanto dos produtos, como os dados do formato da tabela.
 	function generateTable(){
 	    
-		// Recupera o elemento em que as "THs" ser„o embutidas.
-		var tr = document.getElementById("tabela_de_produtos")
-		                 .firstElementChild
-		                 .firstElementChild
-		                 .firstElementChild;
+		// Recupera o elemento em que as "THs" ser√£o embutidas.
+		var tr_head = document.getElementById("cabecalho_da_tabela")
 		
-		// O "_head" no fim, significa que essas vari·veis
-		// dizem ‡ respeito do cabeÁalho da tabela.
+		// Recupera o elemento "tbody" para inserir os elementos
+		// "tr"s que ser√£o criados.
+		var table_body = document.getElementById("corpo_da_tabela");
+		
+		// O "_head" no fim, significa que essas vari√°veis
+		// dizem √† respeito do cabe√ßalho da tabela.
         var td_head, tdElem_head; 
 
-		// Esse loop serve para construir o cabeÁalho da tabela.
+		// Esse loop serve para construir o cabe√ßalho da tabela.
 		for(var a=0; a < table_columns.length; a++){
 		    td_head = table_columns[a];
 			tdElem_head = document.createElement("td");
 			tdElem_head.innerHTML = td_head;
-			tr.appendChild(tdElem);
+			tr_head.appendChild(tdElem_head);
 		}
 		
-		// O "_body" no fim, significa que essas vari·veis
-		// dizem ‡ respeito do corpo da tabela.
+		// O "_body" no fim, significa que essas vari√°veis
+		// dizem √† respeito do corpo da tabela.
 		var td_body, tdElem_body, trElem_body, product_data;
 		
 		// Esse loop constroi os dados da tabela, baseado no 
-		// cabeÁalho.
+		// cabe√ßalho.
 		for(var b=0; b < table_products.length; b++){
 		    
 			// Para cada produto, cria um elemento "tr" na tabela.
 			trElem_body = document.createElement("tr");
 			
-			// Essa vari·vel guarda um array com os dados do
+			// Essa vari√°vel guarda um array com os dados do
 			// produto.
 			product_data = table_products[b];
 			
-		    // Esse loop itera atravÈs do cabeÁalho da tabela,
-			// para saber a ordem dos dados ‡ serem inseridos.
+		    // Esse loop itera atrav√©s do cabe√ßalho da tabela,
+			// para saber a ordem dos dados √† serem inseridos.
 		    for(var c=0; c < table_columns.length; c++){
 			    
 				// Cria o elemento td com o dado correspondente no 
 				// array de dados: "table_products".
 				td_body = product_data[c];
+				
+				// Verifica se a vari√°vel "td_body" possui um valor 
+				// verdadeiro e v√°lido.
+				if(!td_body){
+				    td_body = "";
+				}
+				
 				tdElem_body = document.createElement("td");
 				tdElem_body.innerHTML = td_body;
-				
-				
+                
+                // Anexa o elemento "td" dentro do "tr".
+                trElem_body.appendChild(tdElem_body);				    
 				
 			}
-			
+            
+			// Anexa o elemento "tr" que foi formado ao 
+			// fim da tabela.
+			table_body.appendChild(trElem_body);
+		
 		}
 		
 	}
 	
 	generateTable();
 	
-	// Quando o programa È finalizado, salva os dados
+	// Quando o programa √© finalizado, salva os dados
 	// fornecidos ao programa durante o uso.
 	window.beforeunload = function(){
 	    window.localStorage.table_columns = JSON.stringify(table_columns);
